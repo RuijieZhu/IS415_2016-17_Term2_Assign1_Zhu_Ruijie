35e6fb71e1c256124d34d3a9507a8dc91ee91a50
RuijieZhu <ruijie.zhu.2014@sis.smu.edu.sg>
2017-02-26 12:14:19 +0800

    updated version

---
+++ js/qgis2web_expressions.js
@@ -0,0 +1,64 @@
+
+
+
+function HDBBranchesgyms2rule0_eval_expression(context) {
+    // Composite_HDBs_gyms_NUMPOINTS = 1
+
+    var feature = context.feature;
+    
+    return (feature.properties['Composite_HDBs_gyms_NUMPOINTS']  == 1);
+}
+
+
+function HDBBranchesgyms2rule1_eval_expression(context) {
+    // Composite_HDBs_gyms_NUMPOINTS = 2
+
+    var feature = context.feature;
+    
+    return (feature.properties['Composite_HDBs_gyms_NUMPOINTS']  == 2);
+}
+
+
+function HDBBranchesgyms2rule2_eval_expression(context) {
+    // Composite_HDBs_gyms_NUMPOINTS = 3
+
+    var feature = context.feature;
+    
+    return (feature.properties['Composite_HDBs_gyms_NUMPOINTS']  == 3);
+}
+
+
+function HDBBranchesgyms2rule3_eval_expression(context) {
+    // Composite_HDBs_gyms_NUMPOINTS = 4
+
+    var feature = context.feature;
+    
+    return (feature.properties['Composite_HDBs_gyms_NUMPOINTS']  == 4);
+}
+
+
+function HDBBranchesgyms2rule4_eval_expression(context) {
+    // Composite_HDBs_gyms_NUMPOINTS = 5
+
+    var feature = context.feature;
+    
+    return (feature.properties['Composite_HDBs_gyms_NUMPOINTS']  == 5);
+}
+
+
+function HDBBranchesgyms2rule5_eval_expression(context) {
+    // Composite_HDBs_gyms_NUMPOINTS = 9
+
+    var feature = context.feature;
+    
+    return (feature.properties['Composite_HDBs_gyms_NUMPOINTS']  == 9);
+}
+
+
+function HDBBranchesgyms2rule6_eval_expression(context) {
+    // Composite_HDBs_gyms_NUMPOINTS = 16
+
+    var feature = context.feature;
+    
+    return (feature.properties['Composite_HDBs_gyms_NUMPOINTS']  == 16);
+}
